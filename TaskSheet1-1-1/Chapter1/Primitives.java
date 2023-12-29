febc5e99941ef539c696c4ac5c7cdc379b8ddbb3
Rogerick Aban <96326024+erickaban14@users.noreply.github.com>
2023-12-29 07:37:32 +0000

    test

---
+++ TaskSheet1-1-1/Chapter1/Primitives.java
@@ -0,0 +1,18 @@
+package Chapter1;
+
+public class Primitives {
+    public static void main(String[] args) {
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        String IF = "Hello";
+        System.out.println(IF);
+    }
+
+    int notValid() {
+        int y = 10;
+        int x;
+        int reply = x + y;
+        return reply;
+    }
+
+}
